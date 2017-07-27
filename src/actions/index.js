9d80a7f6df353273eea467313427493596788495
Branimir123 <brani_svetoslavov@abv.bg>
2017-07-27 15:28:49 +0300

    URL change

--- src/actions/index.js
+++ src/actions/index.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 import { FETCH_WEATHER, FETCH_WEATHER_ERROR } from './types';
 
 const API_KEY  = process.env.API_KEY;
-const ROOT_URL = `http://api.openweathermap.org/data/2.5/weather?appid=${API_KEY}`;
+const ROOT_URL = `https://api.openweathermap.org/data/2.5/weather?appid=${API_KEY}`;
 
 export default function fetchWeather(coordinates) {
     return function(dispatch){        
