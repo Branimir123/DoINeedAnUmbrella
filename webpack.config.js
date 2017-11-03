74caa8c6dd483445bc5868c08cd146fb86b2bc18
Branimir123 <brani_svetoslavov@abv.bg>
2017-11-03 13:41:53 +0200

    Fix webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,5 @@
+var webpack = require("webpack");
+
 module.exports = {
   entry: [
     './src/index.js'
@@ -22,5 +24,12 @@ module.exports = {
   devServer: {
     historyApiFallback: true,
     contentBase: './'
-  }
+  },
+  plugins: [
+      new webpack.DefinePlugin({
+          "process.env": {
+            API_KEY: process.env.API_KEY
+          }
+      })
+  ]
 };
